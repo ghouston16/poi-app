261535466dc28f2d8bb99f2a750bb90bc08abb59
ghouston16 <ghouston16@hotmail.com>
2021-05-31 22:21:46 +0100

    undo previous commit

--- app/api/pois.js
+++ app/api/pois.js
@@ -42,7 +42,7 @@ const Pois = {
     auth: {
       strategy: "jwt",
     },
-
+    /*
    validate: {
       payload: {
         name: Joi.string().required(), //.regex(/^[A-Z][a-z]{2,}$/),
@@ -50,12 +50,11 @@ const Pois = {
         category: Joi.string().required(),
         lat: Joi.string().required(),
         long: Joi.string().required(),
-        image: Joi.string(),
+        image: Joi.string().required(false),
        // creator: Joi.string().required()
       },
     },
-
-    
+    */
     handler: async function (request, h) {
       const userId = utils.getUserIdFromRequest(request);
       let poi = new Poi(request.payload);
@@ -94,7 +93,7 @@ const Pois = {
     auth: {
       strategy: "jwt",
     },
-
+/*
     validate: {
       payload: {
         name: Joi.string().required().regex(/^[A-Z][a-z]{2,}$/),
@@ -106,7 +105,7 @@ const Pois = {
       },
     },
 
-
+ */
     handler: async function(request, h) {
       try {
         const poiEdit = request.payload;
